f37d4f78faf60a11999bfc1b3db6494951bbc4fe
Andres Barro <andresbarroe+git@gmail.com>
2017-10-04 20:29:04 -0500

    Dining Philosophers 1

---
+++ threads/phil.c
@@ -0,0 +1,81 @@
+/******************************************************************************
+* FILE: hello.c
+* DESCRIPTION:
+*   A "hello world" Pthreads program.  Demonstrates thread creation and
+*   termination.
+* AUTHOR: Blaise Barney
+* LAST REVISED: 08/09/11
+******************************************************************************/
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <time.h>
+
+#define NUM_PHIL 5
+#define NUM_FORK 5
+
+sem_t forks[NUM_FORK];
+
+int get_rand(float max)
+{
+    return 1 + (rand() * max) / RAND_MAX;
+}
+
+void thinking()
+{
+    sleep(get_rand(5.0));
+}
+
+void eating(void)
+{
+    sleep(get_rand(5.0));
+}
+
+void *phil_live(void *threadid)
+{
+    long tid;
+    tid = (long)threadid;
+    for (;;)
+    {
+        printf("Im phil. #%ld thinking.\n", tid);
+        thinking();
+        if (tid % 2 == 0)
+        {
+            sem_wait(&forks[tid]);
+            sem_wait(&forks[(tid + 1) % NUM_PHIL]);
+        }
+        else
+        {
+            sem_wait(&forks[(tid + 1) % NUM_PHIL]);
+            sem_wait(&forks[tid]);
+        }
+        printf("Im phil. #%ld eating.\n", tid);
+        eating();
+        sem_post(&forks[tid]);
+        sem_post(&forks[(tid + 1) % NUM_PHIL]);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    pthread_t phils[NUM_PHIL];
+    int rc;
+    long t;
+    for (int i = 0; i < NUM_FORK; ++i)
+        sem_init(&forks[i], 0, 1);
+    for (t = 0; t < NUM_PHIL; t++)
+    {
+        // printf("In main: creating thread %ld\n", t);
+        rc = pthread_create(&phils[t], NULL, phil_live, (void *)t);
+        if (rc)
+        {
+            printf("ERROR; return code from pthread_create() is %ld\n", rc);
+            exit(-1);
+        }
+    }
+
+    pthread_exit(NULL);
+}
